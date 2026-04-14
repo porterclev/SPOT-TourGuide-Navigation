4e849511baaaa9d0ce41037d5b0ae492d7f550e1
porterclev <porterclev@gmail.com>
2026-04-13 21:00:51 -0700

    structure

---
+++ tour_nav.py
@@ -0,0 +1,65 @@
+import argparse
+import logging
+import math
+import os
+import sys
+import time
+import traceback
+
+import google.protobuf.timestamp_pb2
+import graph_nav_sdk.graph_nav_util as graph_nav_util
+from graph_nav_sdk.graph_nav_command_line import GraphNavInterface
+import grpc
+
+import bosdyn.client.channel
+import bosdyn.client.util
+from bosdyn.api import geometry_pb2, power_pb2, robot_state_pb2
+from bosdyn.api.gps import gps_pb2
+from bosdyn.api.graph_nav import graph_nav_pb2, map_pb2, nav_pb2
+from bosdyn.client.exceptions import ResponseError
+from bosdyn.client.frame_helpers import get_odom_tform_body
+from bosdyn.client.graph_nav import GraphNavClient
+from bosdyn.client.lease import LeaseClient, LeaseKeepAlive, ResourceAlreadyClaimedError
+from bosdyn.client.math_helpers import Quat, SE3Pose
+from bosdyn.client.power import PowerClient, power_on_motors, safe_power_off_motors
+from bosdyn.client.robot_command import RobotCommandBuilder, RobotCommandClient
+from bosdyn.client.robot_state import RobotStateClient
+
+def main():
+    """Run the command-line interface."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-u', '--upload-filepath',
+                        help='Full filepath to graph and snapshots to be uploaded.', required=True)
+    parser.add_argument(
+        '-g', '--use-gps', action='store_true', help=
+        'Enable GPS commands for this robot. The robot must have a GPS payload. The map must have been recorded with GPS.'
+    )
+    bosdyn.client.util.add_base_arguments(parser)
+    options = parser.parse_args()
+
+    # Setup and authenticate the robot.
+    sdk = bosdyn.client.create_standard_sdk('GraphNavClient')
+    robot = sdk.create_robot(options.hostname)
+    bosdyn.client.util.authenticate(robot)
+
+    graph_nav_command_line = GraphNavInterface(robot, options.upload_filepath, options.use_gps)
+    lease_client = robot.ensure_client(LeaseClient.default_service_name)
+    try:
+        with LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
+            try:
+                graph_nav_command_line.run()
+                return True
+            except Exception as exc:  # pylint: disable=broad-except
+                print(exc)
+                print('Graph nav command line client threw an error.')
+                return False
+    except ResourceAlreadyClaimedError:
+        print(
+            'The robot\'s lease is currently in use. Check for a tablet connection or try again in a few seconds.'
+        )
+        return False
+
+
+if __name__ == '__main__':
+    if not main():
+        sys.exit(1)
